dcecf678b3bd3b2bd24bbb303cfd1e03bd2702c0
Nabor <naborlopezjimenez@gmail.com>
2016-12-11 21:40:32 +0000

    Cambios de Style.css

--- header.php
+++ header.php
@@ -1,6 +1,11 @@
 <html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri();?>"/>
+    </head>
     <body>
+        <div id="icono">
         <a href="https://temasxenabor-naborlj.c9users.io/">
         <img src="<?php echo get_template_directory_uri()?>/images/icono.png">
+        </div>
       
 
