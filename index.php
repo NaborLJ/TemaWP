e62613ee8ab0c3e3052308000c5b3d8a47dfccb8
Nabor <naborlopezjimenez@gmail.com>
2016-12-11 20:26:26 +0000

    Index añadido / Muestra post en caso de haberlos.

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php get_header(); ?>
+    <p><h1>Hello to my web </h1></p>
+        <?php 
+        if (have_posts()) {
+         while (have_posts()) {
+            the_post(); ?>
+            <h2 id="post-<?php the_ID(); ?>">
+            <a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>">
+            <?php the_title(); ?></a></h2>
+            <small><?php the_time('F jS, Y') ?>  by <?php the_author() ?> </small>
+            <?php the_content();
+         }
+        }?>
+<?php get_footer();?>
