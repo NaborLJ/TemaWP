ba4a6fc612eb3ed568fe9710f48947ad9cb81086
Nabor <naborlopezjimenez@gmail.com>
2016-12-11 20:40:48 +0000

    Header mostrará un icono redirecionador

--- header.php
+++ header.php
@@ -1,6 +1,7 @@
 <html>
     <body>
-        <h1>Bienvenido a mi WebSite</h1>
-
+        <h1>ENJOY</h1>
+        <a href="https://temasxenabor-naborlj.c9users.io/">
+        <img src="<?php echo get_template_directory_uri()?>/images/icono.png">
 
 
